1f866dfd2d1192c8ef59d51baef2b931fba72c1e
Ezioleq <ezioleq@wp.pl>
2021-04-01 23:06:28 +0200

    Something I don't want to lose aka CompoundTags

--- src/main/java/net/ezioleq/voidinary/energy/IEnergy.java
+++ src/main/java/net/ezioleq/voidinary/energy/IEnergy.java
@@ -1,5 +1,8 @@
 package net.ezioleq.voidinary.energy;
 
+import net.ezioleq.voidinary.Voidinary;
+import net.minecraft.item.ItemStack;
+
 public interface IEnergy {
 	/**
 	 * 1 VoidFlux = 1000 units
@@ -7,25 +10,30 @@ public interface IEnergy {
 	public static final int VOIDFLUX = 1000;
 
 	/**
+	 * Our tag for energy
+	 */
+	public static final String TAG_KEY = Voidinary.MODID + "energy";
+
+	/**
 	 * @return Amount of energy stored in object
 	 */
-	int getEnergy();
+	int getEnergy(ItemStack stack);
 
 	/**
 	 * Set amount of energy stored by object
 	 * @param amount
 	 */
-	void setEnergy(int amount);
+	void setEnergy(ItemStack stack, int amount);
 
 	/**
 	 * Subtract given amount of energy from object
 	 * @param amount
 	 */
-	void subtractEnergy(int amount);
+	void subtractEnergy(ItemStack stack, int amount);
 
 	/**
 	 * Add given amount of energy to object
 	 * @param amount
 	 */
-	void addEnergy(int amount);
+	void addEnergy(ItemStack stack, int amount);
 }
--- src/main/java/net/ezioleq/voidinary/items/Battery.java
+++ src/main/java/net/ezioleq/voidinary/items/Battery.java
@@ -7,58 +7,75 @@ import net.ezioleq.voidinary.utils.Utils;
 import net.ezioleq.voidinary.Voidinary;
 import net.ezioleq.voidinary.energy.IEnergy;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 public class Battery extends Item implements IEnergy, IDurabilityBar {
 	int capacity = 0;
-	int storedEnergy = 500;
 	BatteryStatus status = BatteryStatus.IDLE;
 
 	public Battery(int capacity) {
 		super(new Item.Settings().group(Voidinary.ITEM_GROUP).maxCount(1));
 		this.capacity = capacity;
-		this.storedEnergy = capacity/2; // FIXME: Test purposes only
 	}
 
 	@Override
 	public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
 		tooltip.add(new TranslatableText("item.voidinary.battery.tooltip",
-				Utils.getPercentString(this.storedEnergy, this.capacity) + "%"));
+				Utils.getPercentString(getEnergy(stack), this.capacity) + "%"));
+	}
+
+	@Override
+	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+		// TODO: Do you seriously think we will charge batteries by shaking them?
+		ItemStack stack = user.getStackInHand(hand); 
+		if (getEnergy(stack) < capacity) {
+			addEnergy(stack, 10);
+		}
+		return TypedActionResult.success(stack);
 	}
 
 	// IEnergy
 	@Override
-	public int getEnergy() {
-		return storedEnergy;
+	public int getEnergy(ItemStack stack) {
+		CompoundTag tag = stack.getOrCreateTag();
+		if (tag.contains(IEnergy.TAG_KEY))
+			return tag.getInt(TAG_KEY);
+		else {
+			tag.putInt(TAG_KEY, 0);
+			return 0;
+		}
 	}
 
 	@Override
-	public void setEnergy(int amount) {
-		storedEnergy = amount;
+	public void setEnergy(ItemStack stack, int amount) {
+		CompoundTag tag = stack.getOrCreateTag();
+		tag.putInt(TAG_KEY, amount);
 	}
 
 	@Override
-	public void subtractEnergy(int amount) {
-		storedEnergy -= amount;
-		if (storedEnergy < 0)
-			storedEnergy = 0;
+	public void subtractEnergy(ItemStack stack, int amount) {
+		CompoundTag tag = stack.getOrCreateTag();
+		tag.putInt(TAG_KEY, Utils.clampi(tag.getInt(TAG_KEY) - amount, 0, capacity));
 	}
 
 	@Override
-	public void addEnergy(int amount) {
-		storedEnergy += amount;
-		if (storedEnergy > capacity)
-			storedEnergy = capacity;
+	public void addEnergy(ItemStack stack, int amount) {
+		CompoundTag tag = stack.getOrCreateTag();
+		tag.putInt(TAG_KEY, Utils.clampi(tag.getInt(TAG_KEY) + amount, 0, capacity));
 	}
 
 	// IDurability
 	@Override
 	public double getDurability(ItemStack stack) {
-		return (float)storedEnergy/(float)capacity;
+		return (float)getEnergy(stack)/(float)capacity;
 	}
 
 	@Override
