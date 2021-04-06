30b0f4b2baa508495a82ddfb2d7e098949fa7039
Ezioleq <ezioleq@wp.pl>
2021-04-06 16:03:08 +0200

    Add Heat Generator facing direction

--- src/main/java/net/ezioleq/voidinary/blocks/HeatGenerator.java
+++ src/main/java/net/ezioleq/voidinary/blocks/HeatGenerator.java
@@ -1,15 +1,32 @@
 package net.ezioleq.voidinary.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.Material;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager.Builder;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.Direction;
 
-public class HeatGenerator extends Block {
+public class HeatGenerator extends HorizontalFacingBlock {
 	public HeatGenerator() {
 		super(Settings.of(Material.METAL).strength(4, 8));
+		setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
+	}
+
+	@Override
+	protected void appendProperties(Builder<Block, BlockState> builder) {
+		builder.add(Properties.HORIZONTAL_FACING);
+	}
+
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		Direction dir = Direction.fromHorizontal(ctx.getPlayerFacing().getOpposite().getHorizontal());
+		return getDefaultState().with(Properties.HORIZONTAL_FACING, dir);
 	}
 
 	// TODO: Crafting
-	// TODO: Block rotation
 	// TODO: GUI
 	// TODO: Energy generation
 	// TODO: Other stuff I forgot to write here
