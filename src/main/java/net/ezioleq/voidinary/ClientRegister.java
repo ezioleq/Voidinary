f0c4c679e77a35dcca7553fd676f78a0c9462ef9
Ezioleq <ezioleq@wp.pl>
2021-04-17 19:44:38 +0200

    Add issues url

--- src/main/java/net/ezioleq/voidinary/ClientRegister.java
+++ src/main/java/net/ezioleq/voidinary/ClientRegister.java
@@ -2,8 +2,11 @@ package net.ezioleq.voidinary;
 
 import net.ezioleq.voidinary.gui.HeatGeneratorGui;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 
+@Environment(EnvType.CLIENT)
 public class ClientRegister implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
