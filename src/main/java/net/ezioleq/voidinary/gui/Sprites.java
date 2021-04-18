1ffbb4a8086cb8043277c4a7bbbbbfc46ee688ea
Ezioleq <ezioleq@wp.pl>
2021-04-18 16:40:53 +0200

    Add tanks sprites

--- src/main/java/net/ezioleq/voidinary/gui/Sprites.java
+++ src/main/java/net/ezioleq/voidinary/gui/Sprites.java
@@ -6,4 +6,7 @@ import net.minecraft.util.Identifier;
 public class Sprites {
 	public static final Identifier FLAME = new Identifier(Voidinary.MODID, "textures/gui/flame.png");
 	public static final Identifier FLAME_BG = new Identifier(Voidinary.MODID, "textures/gui/flame_bg.png");
+	public static final Identifier TANK_BG = new Identifier(Voidinary.MODID, "textures/gui/tank_bg.png");
+	public static final Identifier TANK_ENERGY = new Identifier(Voidinary.MODID, "textures/gui/tank_energy.png");
+	public static final Identifier TANK_FUEL = new Identifier(Voidinary.MODID, "textures/gui/tank_fuel.png");
 }
