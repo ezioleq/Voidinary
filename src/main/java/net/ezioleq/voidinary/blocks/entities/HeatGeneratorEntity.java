aae453686f7795d8ae37ef20dad0ae59d6638f75
Ezioleq <ezioleq@wp.pl>
2021-04-18 17:33:32 +0200

    Add bars and inventory to Heat Generator

--- src/main/java/net/ezioleq/voidinary/blocks/entities/HeatGeneratorEntity.java
+++ src/main/java/net/ezioleq/voidinary/blocks/entities/HeatGeneratorEntity.java
@@ -1,13 +1,31 @@
 package net.ezioleq.voidinary.blocks.entities;
 
+import io.github.cottonmc.cotton.gui.PropertyDelegateHolder;
 import net.ezioleq.voidinary.VRegister;
 import net.ezioleq.voidinary.Voidinary;
 import net.ezioleq.voidinary.energy.IEnergy;
+import net.ezioleq.voidinary.utils.IDefaultInventory;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.InventoryProvider;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.SidedInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.screen.PropertyDelegate;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.WorldAccess;
 
-public class HeatGeneratorEntity extends BlockEntity implements IEnergy {
+public class HeatGeneratorEntity extends BlockEntity implements IEnergy, PropertyDelegateHolder, IDefaultInventory, InventoryProvider, SidedInventory {
 	final int capacity = Voidinary.config.heatGeneratorCapacity;
+
+	DefaultedList<ItemStack> inventory = DefaultedList.ofSize(2, ItemStack.EMPTY);
+	static final int[] INPUT_SLOTS = {0};
+	static final int[] OUTPUT_SLOTS = {1};
+	int fuel = 0;
+	int maxFuel = 1000;
 
 	public HeatGeneratorEntity() {
 		super(VRegister.HEAT_GENERATOR_ENTITY);
@@ -26,5 +44,90 @@ public class HeatGeneratorEntity extends BlockEntity implements IEnergy {
 	@Override
 	public int getMaxEnergy(Direction dir) {
 		return capacity;
+	}
+
+	@Override
+	public DefaultedList<ItemStack> getItems() {
+		return inventory;
+	}
+
+	@Override
+	public SidedInventory getInventory(BlockState state, WorldAccess world, BlockPos pos) {
+		return this;
+	}
+
+	@Override
+	public int[] getAvailableSlots(Direction side) {
+		return side == Direction.DOWN ? OUTPUT_SLOTS : INPUT_SLOTS;
+	}
+
+	@Override
+	public boolean canExtract(int slot, ItemStack stack, Direction dir) {
+		return slot == OUTPUT_SLOTS[0];
+	}
+
+	@Override
+	public boolean canInsert(int slot, ItemStack stack, Direction dir) {
+		if (slot == OUTPUT_SLOTS[0])
+			return false;
+		return true;
+	}
+
+	@Override
+	public CompoundTag toTag(CompoundTag tag) {
+		Inventories.toTag(tag, inventory);
+		tag.putInt("fuel", fuel);
+		tag.putInt("maxFuel", maxFuel);
+		return super.toTag(tag);
+	}
+
+	@Override
+	public void fromTag(BlockState state, CompoundTag tag) {
+		super.fromTag(state, tag);
+		Inventories.fromTag(tag, inventory);
+		fuel = tag.getInt("fuel");
+		maxFuel = tag.getInt("maxFuel");
+	}
+
+	PropertyDelegate propdel = new PropertyDelegate() {
+		@Override
+		public int size() {
+			return 4;
+		};
+
+		@Override
+		public void set(int index, int value) {
+			switch (index) {
+				case 0:
+				case 1:
+					break;
+				case 2:
+					fuel = value;
+					break;
+				case 3:
+					maxFuel = value;
+					break;
+			}
+		};
+
+		@Override
+		public int get(int index) {
+			switch (index) {
+				case 0:
+					return getEnergy(Direction.UP);
+				case 1:
+					return getMaxEnergy(Direction.UP);
+				case 2:
+					return fuel;
+				case 3:
+					return maxFuel;
+			}
+			return 0;
+		};
+	};
+
+	@Override
+	public PropertyDelegate getPropertyDelegate() {
+		return propdel;
 	}
 }
--- src/main/java/net/ezioleq/voidinary/gui/HeatGeneratorGui.java
+++ src/main/java/net/ezioleq/voidinary/gui/HeatGeneratorGui.java
@@ -2,14 +2,19 @@ package net.ezioleq.voidinary.gui;
 
 import io.github.cottonmc.cotton.gui.SyncedGuiDescription;
 import io.github.cottonmc.cotton.gui.client.CottonInventoryScreen;
+import io.github.cottonmc.cotton.gui.widget.WBar;
 import io.github.cottonmc.cotton.gui.widget.WGridPanel;
 import io.github.cottonmc.cotton.gui.widget.WItemSlot;
+import io.github.cottonmc.cotton.gui.widget.WLabel;
 import io.github.cottonmc.cotton.gui.widget.WSprite;
+import io.github.cottonmc.cotton.gui.widget.WBar.Direction;
+import io.github.cottonmc.cotton.gui.widget.data.HorizontalAlignment;
 import net.ezioleq.voidinary.VRegister;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
 public class HeatGeneratorGui extends SyncedGuiDescription {
 	public HeatGeneratorGui(int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
@@ -19,11 +24,30 @@ public class HeatGeneratorGui extends SyncedGuiDescription {
 		WGridPanel root = new WGridPanel();
 		setRootPanel(root);
 
-		WSprite flame = new WSprite(Sprites.FLAME_BG);
-		root.add(flame, 4, 1);
+		// Header label
+		WLabel label = new WLabel(new TranslatableText("block.voidinary.heat_generator"));
+		label.setHorizontalAlignment(HorizontalAlignment.CENTER);
+		root.add(label, 0, 0, 9, 1);
 
+		// Left energy bar
+		WBar energyBar = new WBar(Sprites.TANK_BG, Sprites.TANK_ENERGY, 0, 1, Direction.UP);
+		root.add(energyBar, 0, 0, 1, 4);
+
+		// Right fuel bar
+		WBar fuelBar = new WBar(Sprites.TANK_BG, Sprites.TANK_FUEL, 2, 3, Direction.UP);
+		root.add(fuelBar, 8, 0, 1, 4);
+
+		// Flame above input slot
+		WSprite flame = new WSprite(Sprites.FLAME_BG);
+		root.add(flame, 3, 1);
+
+		// Input slot
 		WItemSlot input = WItemSlot.of(blockInventory, 0);
-		root.add(input, 4, 2);
+		root.add(input, 3, 2);
+
+		// Output slot
+		WItemSlot output = WItemSlot.of(blockInventory, 1);
+		root.add(output, 5, 2);
 
 		root.add(createPlayerInventoryPanel(), 0, 4);
 		root.validate(this);
